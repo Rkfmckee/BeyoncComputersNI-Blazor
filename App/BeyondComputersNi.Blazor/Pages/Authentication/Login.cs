a0d762efeb8ebfeff6e271227ed275eb360202fe
Ryan McKeever <rkfmckee@gmail.com>
2024-03-28 21:42:55 +0000

    Add login page

---
+++ App/BeyondComputersNi.Blazor/Pages/Authentication/Login.cs
@@ -0,0 +1,41 @@
+﻿using BeyondComputersNi.Blazor.ViewModels;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace BeyondComputersNi.Blazor.Pages.Authentication;
+
+public partial class Login
+{
+    public const string PageUrl = "/Login";
+
+    private LoginViewModel LoginViewModel { get; set; }
+    private bool HasErrors { get; set; }
+
+    private bool PasswordIsVisible { get; set; }
+    private InputType PasswordInputType { get; set; } = InputType.Password;
+    private string PasswordVisibilityIcon { get; set; } = Icons.Material.Filled.Visibility;
+
+    private void TogglePasswordVisibility()
+    {
+        PasswordIsVisible = !PasswordIsVisible;
+        PasswordInputType = PasswordIsVisible ? InputType.Text : InputType.Password;
+        PasswordVisibilityIcon = PasswordIsVisible ? Icons.Material.Filled.VisibilityOff : Icons.Material.Filled.Visibility;
+    }
+
+    protected override void OnInitialized()
+    {
+        LoginViewModel = new LoginViewModel();
+    }
+
+    private void OnValidSubmit(EditContext context)
+    {
+        HasErrors = false;
+
+        StateHasChanged();
+    }
+
+    private void OnInvalidSubmit(EditContext context)
+    {
+        HasErrors = true;
+    }
+}
---
+++ App/BeyondComputersNi.Blazor/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeyondComputersNi.Blazor.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    public string? Password { get; set; }
+}
