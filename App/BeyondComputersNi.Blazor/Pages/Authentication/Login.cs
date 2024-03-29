1816ea1381ea40176ff260679d3c3fa4cce333b8
Ryan McKeever <rkfmckee@gmail.com>
2024-03-29 00:20:34 +0000

    Define editcontext

--- App/BeyondComputersNi.Blazor/Pages/Authentication/Login.cs
+++ App/BeyondComputersNi.Blazor/Pages/Authentication/Login.cs
@@ -1,30 +1,46 @@
 ﻿using BeyondComputersNi.Blazor.ViewModels;
 using Microsoft.AspNetCore.Components.Forms;
-using System.ComponentModel.DataAnnotations;
 
 namespace BeyondComputersNi.Blazor.Pages.Authentication;
 
-public partial class Login
+public partial class Login : IDisposable
 {
     public const string PageUrl = "/Login";
 
     private LoginViewModel? LoginViewModel { get; set; }
+    private EditContext? EditContext { get; set; }
     private bool HasErrors { get; set; }
 
     protected override void OnInitialized()
     {
         LoginViewModel = new LoginViewModel();
+        EditContext = new EditContext(LoginViewModel);
+        EditContext.OnFieldChanged += HandleFieldChanged;
+    }
+
+    private void HandleFieldChanged(object? sender, FieldChangedEventArgs e)
+    {
+        if (EditContext is null) return;
+
+        HasErrors = !EditContext.Validate();
     }
 
     private void OnValidSubmit(EditContext context)
     {
         HasErrors = false;
-
         StateHasChanged();
     }
 
     private void OnInvalidSubmit(EditContext context)
     {
         HasErrors = true;
+        StateHasChanged();
+    }
+
+    public void Dispose()
+    {
+        if (EditContext is null) return;
+
+        EditContext.OnFieldChanged -= HandleFieldChanged;
     }
 }
