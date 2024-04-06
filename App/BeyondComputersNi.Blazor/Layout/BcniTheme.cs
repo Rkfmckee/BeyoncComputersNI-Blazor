996e52d8fc4633e0c02fb6379d6828ec2608dc58
Ryan McKeever <rkfmckee@gmail.com>
2024-04-06 14:20:29 +0100

    Start adding custom theme

---
+++ App/BeyondComputersNi.Blazor/Layout/BcniTheme.cs
@@ -0,0 +1,33 @@
+﻿using MudBlazor;
+
+namespace BeyondComputersNi.Blazor.Layout;
+
+public class BcniTheme
+{
+    public MudTheme Default = new MudTheme
+    {
+        Palette = new PaletteLight
+        {
+            Primary = Colors.LightBlue.Default,
+            Secondary = Colors.Teal.Lighten2,
+            AppbarBackground = Colors.Shades.White,
+            AppbarText = Colors.Grey.Darken4,
+            DrawerBackground = Colors.Shades.White
+        },
+
+        Typography = new Typography
+        {
+
+        },
+
+        LayoutProperties = new LayoutProperties
+        {
+
+        },
+
+        Shadows = new Shadow
+        {
+
+        }
+    };
+}
